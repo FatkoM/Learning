dad3702cba78dead0bc3a0848651eda78f611c31
FatkoM <fatkobatko@abv.bg>
2023-11-02 17:18:24 +0200

    Added the first exercise

---
+++ Homeworks_for_school!/Homework 2/Zadacha2.py
@@ -0,0 +1,20 @@
+stack = []
+
+commands_ammount = int(input("Input the ammount of commands you would like to use!"))
+
+for i in range(0,commands_ammount):
+    command_input =input("Please input your command! (1,2,3,4,5)").split()
+    if "1" in command_input:
+        stack.append(command_input[1])
+    
+    elif "2" in command_input:
+        stack.pop()
+    
+    #Знам че min() и max() правят stack на list, но нямам силата да правя функция която да следи кой е най големия елемнт!!!!!!!!!
+    elif "3" in command_input:
+        print(max(stack))
+    
+    else:
+        print((min(stack)))
+for i in stack:
+    print(i)
