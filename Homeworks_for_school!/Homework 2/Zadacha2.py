2a17e9e74087ed6a65af4064e32a45d109015261
FatkoM <fatkobatko@abv.bg>
2023-11-07 15:09:45 +0200

    Fixed issues
    Issues with functions min and max were fixed.

--- Homeworks_for_school!/Homework 2/Zadacha2.py
+++ Homeworks_for_school!/Homework 2/Zadacha2.py
@@ -1,22 +1,45 @@
 stack = []
 
+biggest_num = 0
+smallest_num = 0
 commands_ammount = int(input("Input the ammount of commands you would like to use!"))
+
+def max_and_min_calc():
+    global biggest_num
+    global smallest_num
+    for i in stack:
+                if i > biggest_num:
+                    biggest_num = i
+                
+                elif i < smallest_num:
+                    smallest_num = i
 
 for i in range(0,commands_ammount):
     command_input =input("Please input your command! (1,2,3,4,5)").split()
     if "1" in command_input:
-        stack.append(command_input[1])
+        if not stack:
+            biggest_num = int(command_input[1])
+            smallest_num = int(command_input[1])
+        
+        else:
+            max_and_min_calc()
+
+        stack.append(int(command_input[1]))
     
     elif "2" in command_input:
         if stack:
-            stack.pop()
-    
-    #Знам че min() и max() правят stack на list, но нямам силата да правя функция която да следи кой е най големия елемнт!!!!!!!!!
+            if int(command_input[0]) == biggest_num or smallest_num:
+                stack.pop()
+                max_and_min_calc()
+
     elif "3" in command_input:
-        print(max(stack))
+        print(biggest_num)
+    
+    elif "4" in command_input:
+        print(smallest_num)
     
     else:
-        print((min(stack)))
+        print(len(stack))
 
 for i in stack:
     print(i,end=",")
